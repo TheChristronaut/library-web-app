58c92a205b9e43a626141a830f8ebfe657d4e690
Zach <zswiger1997@gmail.com>
2024-04-04 17:35:23 -0500

    Add book with user input

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const library = document.querySelector("#library");
 const newBookBtn = document.querySelector("#new-book-btn");
-const closeFormBtn = document.querySelector("#close-form-btn")
-const newBookForm = document.querySelector("#new-book-dialogue")
+const closeFormBtn = document.querySelector("#close-form-btn");
+const submitFormBtn = document.querySelector("#submit-form-btn");
+const newBookForm = document.querySelector("#new-book-dialogue");
 
 const myLibrary = [];
 myLibrary.sort();
@@ -15,32 +16,44 @@ function Book(title, author, pagesRead, pages, read) {
     return(this.title + " by " + author + ", " + pagesRead + " of " + pages + " pages read, " + read);
 }
 
-function addBookToLibrary() {
-    let newBook = Book("The Unseen Realm", "Michael Heiser" ,"2000", "3000", "not finished yet.")
+function addBookToLibrary(title, author, pagesRead, pages, read) {
+    let newBook = Book(title, author, pagesRead, pages, read)
+    myLibrary.length = 0;
     myLibrary.push(newBook);
+    for (const book of myLibrary) {
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book");
+        bookCard.textContent = book;
+        library.appendChild(bookCard);
+    }
 }
 
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-addBookToLibrary();
-
-for (const book of myLibrary) {
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("book");
-    bookCard.textContent = book;
-    library.appendChild(bookCard);
-}
+addBookToLibrary("A Crown of Ivy and Glass", "Claire LeGrand", "554", "554", "finished.");
 
 newBookBtn.addEventListener("click", () => {
     newBookForm.showModal();
-})
+});
 
 closeFormBtn.addEventListener("click", () => {
+    newBookForm.close();
+});
+
+submitFormBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const titleInput = document.querySelector("#title").value;
+    const authorInput = document.querySelector("#author").value;
+    const pagesReadInput = document.querySelector("#page-location").value;
+    const pagesInput = document.querySelector("#page-number").value;
+    //const readInput = document.querySelector("#read-status").value;
+
+    addBookToLibrary(titleInput, authorInput, pagesReadInput, pagesInput, "finished")
+
+    document.querySelector("#title").value = "";
+    document.querySelector("#author").value = "";
+    document.querySelector("#page-location").value = "";
+    document.querySelector("#page-number").value = "";
+    //document.querySelector("#read-status").value = "";
+
     newBookForm.close();
 });
