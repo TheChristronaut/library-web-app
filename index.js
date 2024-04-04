85fd19498397eb3f42d81b4d0a049c458ac50723
Zach <zswiger1997@gmail.com>
2024-04-04 16:32:29 -0500

    Add dialogue for user input

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const library = document.querySelector("#library");
+const newBookBtn = document.querySelector("#new-book-btn");
+const closeFormBtn = document.querySelector("#close-form-btn")
+const newBookForm = document.querySelector("#new-book-dialogue")
 
 const myLibrary = [];
 myLibrary.sort();
 
-function Book(title, author, pages, read) {
+function Book(title, author, pagesRead, pages, read) {
     this.title = title;
     this.author = author;
+    this.pagesRead = pagesRead;
     this.pages = pages;
     this.read = read;
-    return(this.title + " by " + author + ", " + pages + ", " + read);
+    return(this.title + " by " + author + ", " + pagesRead + " of " + pages + " pages read, " + read);
 }
 
 function addBookToLibrary() {
-    let newBook = Book("The Unseen Realm", "Michael Heiser" , "3000", "not yet read")
+    let newBook = Book("The Unseen Realm", "Michael Heiser" ,"2000", "3000", "not finished yet.")
     myLibrary.push(newBook);
 }
 
 addBookToLibrary();
-
-console.log(myLibrary);
+addBookToLibrary();
+addBookToLibrary();
+addBookToLibrary();
+addBookToLibrary();
+addBookToLibrary();
+addBookToLibrary();
+addBookToLibrary();
+addBookToLibrary();
 
 for (const book of myLibrary) {
     const bookCard = document.createElement("div");
@@ -26,3 +36,11 @@ for (const book of myLibrary) {
     bookCard.textContent = book;
     library.appendChild(bookCard);
 }
+
+newBookBtn.addEventListener("click", () => {
+    newBookForm.showModal();
+})
+
+closeFormBtn.addEventListener("click", () => {
+    newBookForm.close();
+});
