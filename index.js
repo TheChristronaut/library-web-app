c6a314e0020304c2d1a55c24fcf11fd8dfeee43b
Zach <zswiger1997@gmail.com>
2024-04-04 14:51:38 -0500

    Write functions to add book to array

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function() {
-        return(this.title + " by " + author + ", " + pages + ", " + read)
-    }
+    return(this.title + " by " + author + ", " + pages + ", " + read);
 }
 
 function addBookToLibrary() {
-    
+    let newBook = Book("The Unseen Realm", "Michael Heiser" , "3000", "not yet read")
+    myLibrary.push(newBook);
 }
+
+addBookToLibrary();
+
+console.log(myLibrary);
